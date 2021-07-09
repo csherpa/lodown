9aad83f2e0753bf2b4ac0c872d599f7cc1961a27
Ubuntu <ubuntu@ip-172-31-10-72.ec2.internal>
2021-07-09 02:26:36 +0000

    complete lodown

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function identity(value) {
  /**
  * typeOf: JavaScript operator that returns a string indicating the type of value passed.
  * 
- * @param {Value} value: Value of any datatype. 
+ * @param {Value} : Value of any datatype. 
  * @return {String} : Returns a string of indicatiing the type of the value. Eg: 'string', 'array' etc..
  * 
  */
@@ -108,7 +108,7 @@ module.exports.typeOf = typeOf;
  module.exports.last = last;
  
  /**
- * indeOf: Method that returns the first index at which a given element
+ * indeOf: Function that returns the first index at which a given element
  *         can be found in the array, or -1 if it is not present.
  * 
  * @param {Array} collection: The collection over which to iterate.
@@ -130,7 +130,7 @@ module.exports.typeOf = typeOf;
  
   
 /**
- * contains: Function that checks if the array contains a specific value
+ * contains: Function that checks if the array contains a specific value.
  * 
  * @param {Array} collection: The collection over which to iterate.
  * @param {Value} : Can be of any datatype value.
@@ -242,9 +242,9 @@ module.exports.each = each;
  module.exports.reject = reject;
  
  /**
- * partition: Function that iterates over a array to test each element of the array.
- *         Each element that passes to true is placed in trueVal and false values in 
- *         falseVal array;
+ * partition: Function that iterates over an array to test each element of the array.
+ *            Each element that passes to true is placed in trueVal and false values in 
+ *            falseVal array;
  * 
  * @param {Array} array: The array over which to iterate.
  * @param {Funtion} func : Function to test on each element and give a Boolean value.
@@ -277,7 +277,7 @@ module.exports.each = each;
  *      func to each element in the collection.
  * 
  * @param {Collection} : The collection over which to iterate.
- * @param {Funtion} func : Function that is called for every element of arr. 
+ * @param {Funtion} func : Function that is called for every element of array. 
  *                        Each time func executes, the returned value is added to newArray
  * 
  * @return {Array} : New array with each element being the result of the callback function
@@ -298,11 +298,12 @@ module.exports.each = each;
  * pluck : Function that iterates over the collection and checks if object has the property.
  *       If it does push the matching property into a new array.
  * 
- * @param {Array} : The array of object over which to iterate.
- * @param {Property} : Function that is called for every element of arr. 
- *                   Each time func executes, the returned value is added to newArray
+ * @param {Array} : The array over which to iterate.
  * 
- * @return {Array} : New array containing the value of property for every element in array.
+ * @param {Property} : The property from object in the array that will be added into
+ *                     a new array.
+ * 
+ * @return {Array} : Returns new array containing the value that matches the property of the array.
  * 
  */
  
@@ -316,3 +317,144 @@ module.exports.each = each;
  }
  
  module.exports.pluck = pluck;
+ 
+ /**
+ * every : Function that tests whether all elements in the array pass the test implemented by the
+ *        provided function. It returns true when all elements pass else false.
+ * 
+ * @param {Collection} : The collection over which to iterate.
+ * @param {Function} func : Function that will test for all element and return a boolean when all the
+ *                         elements evaluate to true or false.
+ *                      
+ * @return {Boolean} :Return true if the function returns a truthy value for every array element. Otherwise, false.
+ * 
+ */
+ 
+ function every(collection, func) {
+     if(func === undefined){
+       for(let i = 0; i < collection.length; i++){
+           if(!collection[i]){
+               return false;
+           }
+        }
+        return true;
+    }
+    if(Array.isArray(collection)){
+        for(let i = 0; i < collection.length; i++){
+            if(!func(collection[i], i , collection )) {
+              return false;
+            }
+            
+        }
+        return true;
+    }
+    else{
+        for(let key in collection){
+            if(!func(collection[key], key, collection) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+ }
+ 
+ module.exports.every = every;
+ 
+ /**
+ * some : Function that tests whether at least one elements in the array pass the test implemented by the
+ *        provided function. If even one element pass the test it return true else false.       
+ * 
+ * @param {Collection} : The collection over which to iterate.
+ * 
+ * @param {Function} func : Function that test for all elements and return a boolean when at least one
+ *                         elements evaluate to true else false.
+ *                      
+ * @return {Boolean} :Return true if the function returns a truthy value for at least one element. Otherwise, false.
+ * 
+ */
+ 
+ function some(collection, func) {
+     if(func === undefined){
+       for(let i = 0; i < collection.length; i++){
+           if(!collection[i]){
+               return false;
+           }
+        }
+        return true;
+    }
+    if(Array.isArray(collection)){
+        for(let i = 0; i < collection.length; i++){
+            //call func in each values, index and collection 
+            if(func(collection[i], i , collection)){
+                return true;
+            }
+        }
+        return false;
+    }
+    else{
+        for(let key in collection){
+            // console.log(func(collection[key], key, collection));
+            if(func(collection[key], key , collection)){
+                return true;
+            }
+        }
+        return false;
+    }
+ }
+ module.exports.some = some;
+ 
+ /**
+ * reduce : Function that executes a callback function on each element of the array, 
+ *         and returns a single output value. 
+ * 
+ * @param {Array} : The array over which to iterate.
+ * @param {Function} func : A function to execute on each element in the array.
+ *      
+ * @param {Number} seed : A value to use as the first argument to the first call of the callbackFunc.
+ *                       If not given, the first element in the array will be used as the seed.
+ * 
+ * @return {Value} :The single value that results from the reduction.
+ * 
+ */
+ 
+ function reduce(array, func, seed){
+    //check for the arguments.length 
+    for(let i = 1; i < arguments.length; i++){
+      if(arguments.length < 3){
+        //If no initialValue is provided, then accumulator will be equal to the first value in the array
+        seed = array[0];
+        for(let i = 1; i < array.length; i++){
+            //seed is the accumulator;
+            seed = func(seed, array[i], i);
+        }
+        return seed;
+      }
+      else{
+          for(let i = 0; i < array.length; i++){
+              seed = func(seed, array[i], i);
+          }
+          return seed;
+      }
+    }
+ }
+ module.exports.reduce = reduce;
+ 
+ 
+ /**
+ * extend : Function that iterates over any amount of objects, copies all of its properties 
+ *          from one or more source objects to a target object. Properties in the target object
+ *          are overwritten by properties in the sources if they have the same key.
+ * 
+ * @param {Object} obj1 : The object that will inherit properties.
+ * @param {Object} obj2 : The object from which it will inherit properties.
+ * @param {Objects} ...args : The object from which it will inherit properties.
+ * 
+ * @return {Object} : Returns the modified target object.
+ * 
+ */
+ 
+ function extend(obj1, obj2, ...args) {
+    let newObj = Object.assign(obj1, obj2, ...args);
+    return newObj;
+ }
+ module.exports.extend = extend;
